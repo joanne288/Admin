0c25cf636130d9b1228dadb3b56cfd40759f8ed7
Joanne Mascarenhas <joannemascarenhas8@gmail.com>
2025-06-12 17:32:31 +0530

    minor chnages

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('registrationForm');
-const message = document.getElementById('successMessage');
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  message.style.display = 'block';
-  form.reset();
-});
